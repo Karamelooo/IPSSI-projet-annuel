eb99f684dc06743c722ff762d54dc0d2bbd73ad2
Karamelooo <marmoto@hotmail.fr>
2023-10-24 19:57:04 +0200

    subscribe all parts

--- src/Controller/subscribeController.php
+++ src/Controller/subscribeController.php
@@ -15,4 +15,19 @@ class subscribeController extends AbstractController
     {
         return $this->render('subscribe.html.twig');
     }
+
+    public function subscribe2(): Response
+    {
+        return $this->render('subscribe2.html.twig');
+    }
+	// Green Plan
+    public function subscribe3(): Response
+    {
+        return $this->render('subscribe3.html.twig');
+    }
+	// Paiement
+    public function subscribe4(): Response
+    {
+        return $this->render('subscribe4.html.twig');
+    }
 }
