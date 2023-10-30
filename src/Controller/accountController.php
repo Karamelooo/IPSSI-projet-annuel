ca2d1d3bb23c0e7da409a2b97b89532dfe41dc1e
MerwanDev <mlaakad@yahoo.com>
2023-10-30 17:41:50 +0100

    start account backend

--- src/Controller/accountController.php
+++ src/Controller/accountController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AccountUpdateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class accountController extends AbstractController
 {
 	/**
      * @Route("/account", name="account")
      */
-    public function account(): Response
+       public function account(Request $request, EntityManagerInterface $entityManager)
     {
-        return $this->render('account.html.twig');
+        $user = $this->getUser(); // Récupérez l'utilisateur connecté (assurez-vous que votre système d'authentification est configuré correctement)
+        $form = $this->createForm(AccountUpdateType::class, $user);
+
+        // Gérez la soumission du formulaire ici
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            print_r($user);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigez l'utilisateur vers une page de confirmation ou de profil
+            return $this->redirectToRoute('account'); 
+        }
+        
+        return $this->render('account.html.twig', [
+            'form_account_updated' => $form->createView(),
+        ]);
     }
 }
+
+
+
+
