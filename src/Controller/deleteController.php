73ea5862209f5042cc5bbbadcd1a0c0d7c5a93bb
Karamelooo <marmoto@hotmail.fr>
2023-11-06 02:27:27 +0100

    deleting account + files

--- src/Controller/deleteController.php
+++ src/Controller/deleteController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UploadedFileRepository;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -13,10 +16,28 @@ class deleteController extends AbstractController
 	/**
      * @Route("/delete", name="app_delete_account")
      */
-    public function delete(UserInterface $user, EntityManagerInterface $entityManager): Response
+    public function delete(UserInterface $user, EntityManagerInterface $entityManager, UploadedFileRepository $UploadedFileRepository): Response
     {
         $user = $this->getUser();
         $tokenStorage = $this->container->get('security.token_storage');
+        $userFiles = $UploadedFileRepository->findBy(['user' => $user]);
+        $fileSystem = new Filesystem();
+        $targetDirectory = $this->getParameter('uploads_directory');
+        
+        foreach ($userFiles as $file) {
+            {
+                try {
+                    $fileSystem->remove($targetDirectory . '/' . $file->getFile());
+                } catch (IOExceptionInterface $exception) {
+                    echo "Erreur lors de la suppression du fichier dans " . $exception->getPath();
+                }
+            }
+        }
+        // Supprimez les fichiers de la base de données
+        foreach ($userFiles as $file) {
+            $entityManager->remove($file);
+        }
+
         $tokenStorage->setToken(null);
         $entityManager->remove($user);
         $entityManager->flush();
